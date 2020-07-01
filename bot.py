81d45936f865742ad0583e89f3909ee9c4e239d5
RaniOfTheRain <chloecampbell911@gmail.com>
2020-07-01 20:54:10 +0100

    Added event checking functionality

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ from collections import Counter
 import re
 
 load_dotenv()
+goalReached = False
 TOKEN = os.getenv('DISCORD_TOKEN')
 print(TOKEN)
 POM_TRACK_LIMIT = 10
@@ -21,21 +22,12 @@ MULTILINE_DESCRIPTION_DISABLED = True
 MYSQL_INSERT_QUERY = """INSERT INTO poms (userID, descript, time_set, current_session) VALUES (%s, %s, %s, %s);"""
 MYSQL_EVENT_ADD = """INSERT INTO events (event_name, pom_goal, start_date, end_date) VALUES(%s, %s, %s, %s); """
 MYSQL_SELECT_ALL_POMS = """SELECT * FROM poms WHERE userID= %s"""
+MYSQL_SELECT_EVENT = """ SELECT * FROM events WHERE start_date <= %s AND end_date >= %s;"""
+MYSQL_EVENT_SELECT = """SELECT * FROM poms WHERE time_set >= %s AND time_set <= %s; """
 MYSQL_UPDATE_SESSION = """UPDATE poms SET current_session = 0 WHERE userID= %s AND current_session = 1;"""
 MYSQL_DELETE_POMS = """DELETE FROM poms WHERE userID= %s"""
 bot = commands.Bot(command_prefix='!', case_insensitive=True)
 
-
-'''
-Create connection to database
-'''
-#db = mysql.connector.connect(
-#    host="localhost",
-#    user="admin",
-#   database="pom_bot",
-#    password="KoA1411!!"
-#)
-#cursor = db.cursor(buffered=True)
 """
 Tracks a new pom for the user.
 """
@@ -99,8 +91,30 @@ async def pom(ctx, *, description: str = None):
         f.close()
         print(e)
     await ctx.message.add_reaction("🍅")
+
+    cursor.execute(MYSQL_SELECT_EVENT, (currentDate, currentDate))
+    event_info = cursor.fetchall()
+    event = cursor.rowcount
+
+    if event != 0:
+        
+        cursor.execute(MYSQL_EVENT_SELECT, (event_info[0][3], event_info[0][4]))
+        cursor.fetchall()
+        poms = cursor.rowcount
+        pom_goal = event_info[0][2]
+        
+        if poms >= event_info[0][2] and goalReached == False:
+            await ctx.send("We've reached our goal of %s pom! Well done <@&727974953894543462>!", str(pom_goal))
+        elif goalReached == True:
+            pass
+        else:
+            toSend = "The community has reached " + str(poms) + "/" + str(pom_goal) + " poms. Keep up the good work!"
+            print(toSend)
+            await ctx.send(toSend)
+
     cursor.close()
     db.close()
+    
 
 
 """
@@ -390,10 +404,9 @@ async def start_event(ctx, event_name, event_goal, event_start, event_end):
     cursor.execute(MYSQL_EVENT_ADD, event)
     db.commit()
 
-    # send confirmation message
-
     cursor.close()
     db.close()
+    goalReached = False
 
 
 '''

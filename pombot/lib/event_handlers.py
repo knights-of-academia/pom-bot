d4b86e655a33a29359e386cd65615a5f0a282cc3
Natan Cieplinski <natan.cieplinski.developer@gmail.com>
2021-01-01 22:14:04 +0100

    Send DM if user set timezone before joining (#48)
    * gitignore updated
    
    * Add handler for timezone and emojis in config
    
    * Fixed timezone format
    
    * Query to update timezone added
    
    * Send DM if user set timezone before joining
    
    * Duplicate method removed
    
    * Requested changes implemented

--- pombot/lib/event_handlers.py
+++ pombot/lib/event_handlers.py
@@ -101,10 +101,20 @@ async def on_raw_reaction_add_handler(bot: Bot, payload: RawReactionActionEvent)
         await payload.member.add_roles(role)
 
     if payload.emoji.name in TIMEZONES:
+        user = Storage.get_user_by_id(payload.user_id)
+        if not user:
+            await send_embed_message(
+                None,
+                title=f"Oops! Looks like i had some problem setting your timezone.",
+                description="You first need to join the event",
+                _func=payload.member.send
+            )
+            return
+        
         Storage.set_user_timezone(
-            payload.user_id,
-            timezone(timedelta(hours=TIMEZONES[payload.emoji.name])))
-
+                payload.user_id,
+                timezone(timedelta(hours=TIMEZONES[payload.emoji.name]))
+            )
 
 def _get_guild_team_or_random(guild_id: int) -> Team:
     """Decide which team a user should be on, based on their guild and the

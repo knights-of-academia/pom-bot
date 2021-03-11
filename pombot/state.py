64b1de526549ef013da0a363218f7f65baa19a27
Clay Boone <tener.hades@gmail.com>
2021-03-11 19:00:46 +0000

    quick fix: remove windows-only import

--- pombot/state.py
+++ pombot/state.py
@@ -1,11 +1,8 @@
-from asyncio import ProactorEventLoop
-
-
 class State:
     """In-memory bot state."""
     # Discord creates an event loop for us. Instead of creating a new one, we
     # can hook into the existing event loop to call our storage later.
-    event_loop: ProactorEventLoop = None
+    event_loop = None
 
     # Scoreboard object to preserve and dynamically update scoreboard channels
     # during Pomwar events.

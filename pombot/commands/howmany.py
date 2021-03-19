135311c2c51ca1e11233d02676a0c2aa5c76eca0
Clay Boone <tener.hades@gmail.com>
2021-03-19 00:27:29 +0000

    deprecate and disable 3 commands

--- pombot/commands/howmany.py
+++ pombot/commands/howmany.py
@@ -1,38 +1,44 @@
 # Tech debt: Deprecated March, 2021
+import textwrap
+
 from discord.ext.commands import Context
 
 from pombot.config import Reactions
-from pombot.lib.storage import Storage
-
-WARN = "\n\nThis command will be removed soon. See `!help poms` instead."
 
 
-async def do_howmany(ctx: Context, description: str):
-    """Count your poms with a given description.
+async def do_howmany(ctx: Context):
+    """Deprecated command.
 
-    This command will be removed soon. See `!help poms` instead.
+    This command is disabled and will be removed soon. See `!help poms`
+    instead.
     """
-    if description is None:
-        await ctx.message.add_reaction(Reactions.WARNING)
-        await ctx.send("You must specify a description to search for." + WARN)
-        return
+    await ctx.reply(textwrap.dedent("""\
+        This command has been disabled. See `!help poms` instead.
+    """))
+    await ctx.message.add_reaction(Reactions.ROBOT)
 
-    # Tech debt: `description` could be added to the SQL query for a smaller
-    # network response.
-    poms = await Storage.get_poms(user=ctx.author)
-    matching_poms = [pom for pom in poms if pom.descript == description]
 
-    if not matching_poms:
-        await ctx.message.add_reaction(Reactions.WARNING)
-        await ctx.send("You have no tracked poms with that description." + WARN)
-        return
+# FIXME
+# async def do_howmany(ctx: Context, description: str):
+#     """Count your poms with a given description."""
+#     if description is None:
+#         await ctx.message.add_reaction(Reactions.WARNING)
+#         await ctx.send("You must specify a description to search for.")
+#         return
 
-    msg = "You have {num_poms} *{description}* pom{s}." + WARN
+#     # Tech debt: `description` could be added to the SQL query for a smaller
+#     # network response.
+#     poms = await Storage.get_poms(user=ctx.author)
+#     matching_poms = [pom for pom in poms if pom.descript == description]
 
-    await ctx.send(
-        msg.format(
-            num_poms=len(matching_poms),
-            description=description,
-            s="" if len(matching_poms) == 1 else "s",
-        ))
-    await ctx.message.add_reaction(Reactions.ABACUS)
+#     if not matching_poms:
+#         await ctx.message.add_reaction(Reactions.WARNING)
+#         await ctx.send("You have no tracked poms with that description.")
+#         return
+
+#     await ctx.message.add_reaction(Reactions.ABACUS)
+#     await ctx.send('You have {num_poms} *"{description}"* pom{s}.'.format(
+#         num_poms=len(matching_poms),
+#         description=description,
+#         s="" if len(matching_poms) == 1 else "s",
+#     ))
--- pombot/commands/reset.py
+++ pombot/commands/reset.py
@@ -1,10 +1,24 @@
+import textwrap
+
 from discord.ext.commands import Context
 
 from pombot.config import Reactions
-from pombot.lib.storage import Storage
 
 
 async def do_reset(ctx: Context):
-    """Permanently deletes all of your poms. This cannot be undone."""
-    await Storage.delete_poms(user=ctx.author)
-    await ctx.message.add_reaction(Reactions.WASTEBASKET)
+    """Deprecated command.
+
+    This command is disabled and will be removed soon. See `!help poms` and
+    `!help bank` instead.
+    """
+    await ctx.reply(textwrap.dedent("""\
+        This command has been disabled. See `!help poms` and `!help bank` instead.
+    """))
+    await ctx.message.add_reaction(Reactions.ROBOT)
+
+
+# FIXME
+# async def do_reset(ctx: Context):
+#     """Permanently deletes all of your poms. This cannot be undone."""
+#     await Storage.delete_poms(user=ctx.author)
+#     await ctx.message.add_reaction(Reactions.WASTEBASKET)
